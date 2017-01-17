ed41d0d7fe0215c688e74f9afe9445bf21cd180f
Zach Krzyzanowski <zkrzyz@gmail.com>
2017-01-17 17:21:05 -0500

    first pass at alpine linux for pg imgage

--- provision.sh
+++ provision.sh
@@ -1,21 +1,10 @@
 #!/bin/bash
 set -e
 
-POSTGRESQL_BIN=/usr/lib/postgresql/9.4/bin/postgres
-POSTGRESQL_CONFIG_FILE=/etc/postgresql/9.4/main/postgresql.conf
-POSTGRESQL_DATA=/var/lib/postgresql/9.4/main
+echo "host all  all    0.0.0.0/0  md5" >> /var/lib/postgresql/data/pg_hba.conf
+# echo "listen_addresses='*'" >> /var/lib/postgresql/data/postgresql.conf
+echo "checkpoint_segments=200" >> /var/lib/postgresql/data/postgresql.conf
+echo "ssl = true" >> /var/lib/postgresql/data/postgresql.conf
+echo "ssl_cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'" >> /var/lib/postgresql/data/postgresql.conf
+echo "ssl_key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'" >> /var/lib/postgresql/data/postgresql.conf
 
-POSTGRESQL_SINGLE="sudo -u postgres $POSTGRESQL_BIN --single --config-file=$POSTGRESQL_CONFIG_FILE"
-
-if [ ! -d $POSTGRESQL_DATA ]; then
-    mkdir -p $POSTGRESQL_DATA
-    chown -R postgres:postgres $POSTGRESQL_DATA
-    sudo -u postgres /usr/lib/postgresql/9.4/bin/initdb -D $POSTGRESQL_DATA -E 'UTF-8'
-fi
-
-# setup database
-$POSTGRESQL_SINGLE <<< "CREATE USER ${DB_USERNAME} WITH PASSWORD '${DB_PASSWORD}';" > /dev/null
-$POSTGRESQL_SINGLE <<< "ALTER USER ${DB_USERNAME} WITH superuser;" > /dev/null
-$POSTGRESQL_SINGLE <<< "CREATE DATABASE ${DB_DATABASE} WITH OWNER=${DB_USERNAME} LC_COLLATE='en_US.utf8' LC_CTYPE='en_US.utf8' ENCODING='UTF8' TEMPLATE=template0;" > /dev/null
-
-exec sudo -u postgres $POSTGRESQL_BIN --config-file=$POSTGRESQL_CONFIG_FILE
