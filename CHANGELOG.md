52fee339b6779d7b715f34dcc7e4a7def3df4127
jonpitch <jon.pitcherella@gmail.com>
2017-01-20 12:32:29 -0500

    trying to chown the ssl [#137831205]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+## 0.0.2
+* Using official `postgres:alpine` image.
+
 ## 0.0.1
 * Initial commit
--- provision.sh
+++ provision.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-chmod 0600 /data/config/ssl/cert.key
+chown postgres:ssl-cert /data/config/ssl
+chown postgres:postgres /data/config/ssl/cert.key
 echo "host all  all    0.0.0.0/0  md5" >> /var/lib/postgresql/data/pg_hba.conf
 echo "checkpoint_segments=200" >> /var/lib/postgresql/data/postgresql.conf
 echo "ssl = true" >> /var/lib/postgresql/data/postgresql.conf
