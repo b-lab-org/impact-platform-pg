8862e533387d54c7ef9a260d4ab4f8427b1bc109
jonpitch <jon.pitcherella@gmail.com>
2017-01-26 10:19:17 -0500

    use local certificates instead of mounted volume

--- provision.sh
+++ provision.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -e
 
-#chown postgres:postgres /data/config/ssl/cert.key
 echo "host all  all    0.0.0.0/0  md5" >> /var/lib/postgresql/data/pg_hba.conf
 echo "checkpoint_segments=200" >> /var/lib/postgresql/data/postgresql.conf
 echo "ssl = true" >> /var/lib/postgresql/data/postgresql.conf
-echo "ssl_cert_file = '/data/config/ssl/cert.crt'" >> /var/lib/postgresql/data/postgresql.conf
-echo "ssl_key_file = '/data/config/ssl/cert.key'" >> /var/lib/postgresql/data/postgresql.conf
+echo "ssl_cert_file = '/ssl/cert.crt'" >> /var/lib/postgresql/data/postgresql.conf
+echo "ssl_key_file = '/ssl/cert.key'" >> /var/lib/postgresql/data/postgresql.conf
