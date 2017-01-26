5ab2bb45a30783b1ff92ecac1d5261bfaeebf5d9
jonpitch <jon.pitcherella@gmail.com>
2017-01-26 09:21:46 -0500

    remove chmod from provision

--- provision.sh
+++ provision.sh
@@ -2,7 +2,6 @@
 set -e
 
 chown postgres:postgres /data/config/ssl/cert.key
-chmod 0600 /data/config/ssl/cert.key
 echo "host all  all    0.0.0.0/0  md5" >> /var/lib/postgresql/data/pg_hba.conf
 echo "checkpoint_segments=200" >> /var/lib/postgresql/data/postgresql.conf
 echo "ssl = true" >> /var/lib/postgresql/data/postgresql.conf
