471804de9b43047ab58a9b4453e9045b29f2b2a7
thematthopkins <mhopkins@bcorporation.net>
2018-03-05 20:23:45 -0500

    postgres 10.2 (#3)

--- provision.sh
+++ provision.sh
@@ -3,7 +3,6 @@ set -e
 
 # connections
 echo "host all  all    0.0.0.0/0  md5" >> /var/lib/postgresql/data/pg_hba.conf
-echo "checkpoint_segments=200" >> /var/lib/postgresql/data/postgresql.conf
 
 # turn on ssl
 cd "${PGDATA}"
